8e46d6e20994f7f4e291fb3c7b20dcd28e64a369
Mahesh Bambhaniya <maheshbambhaniya2003@gmail.com>
2023-09-06 12:10:37 +0530

    today commit

--- data/request/create_tag_req.go
+++ data/request/create_tag_req.go
@@ -1,6 +1,6 @@
 package request
 
 type CreateTagsRequest struct{
-	ItemName string `validate:"required, min=1,max=200" json="item_name"`
+	ItemName string `validate:"required, min=1,max=200" json="itemname"`
 	Status string `validate:"required, min=1,max=200" json="status"`
 }
--- data/request/update_tag_req.go
+++ data/request/update_tag_req.go
@@ -2,6 +2,6 @@ package request
 
 type UpdateTagsRequest struct{
 	Id int `validate:"required"`
-    ItemName string `validate:"required, min=1,max=200" json="item_name"`
+    ItemName string `validate:"required, min=1,max=200" json="itemname"`
 	Status string `validate:"required, min=1,max=200" json="status"`
 }
